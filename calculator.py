a3814ea7309142a422fe3f8606a2e9f25dba8785
DakshXSharma <sharmadaksh2315@gmail.com>
2026-06-05 10:19:31 +0530

    Added floor division feature

--- calculator.py
+++ calculator.py
@@ -3,3 +3,13 @@ print("this is only for git practice of branching and PR request.")
 
 def add(a,b):
     return a+b
+
+def floor(a):
+    return floor(a)
+
+def divide(a, b):
+    if(b == 0):
+        print("Error!")
+        return 0
+    else:
+        return a / b
